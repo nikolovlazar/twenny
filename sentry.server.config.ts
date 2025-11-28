6b36f5586e162719136a64720b3be18635b6d830
Lazar Nikolov <hello@nikolovlazar.com>
2025-11-28 12:31:55 -0500

    Add Sentry integration and update dependencies
    - Added Sentry configuration to next.config.ts for error tracking.
    - Included .env.sentry-build-plugin in .gitignore.
    - Updated package.json and package-lock.json to include @sentry/nextjs version 10.27.0.

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a34aa6b273b6c2d7e9782873e396b37e@o4506044970565632.ingest.us.sentry.io/4510443826249728",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
